79d9188cfa2e00364196b2fcd422b41b40b37161
Elizabeth Bailey <ginxy13th@yahoo.com>
2020-06-10 15:55:33 -0400

    added five and six

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ For example removeLastFlavor(originalFlavors) would return ["Rainbow Sherbert", 
 
 function removeLastFlavor(arr){
     arr.pop()
-    console.log(arr)
+    console.log(arr);
 }
 removeLastFlavor(originalFlavors);
 
@@ -106,16 +106,17 @@ Your function should accept:
 
 For example, removeFlavorByName(originalFlavors, "Vanilla") would return an array with the length 30 including all of the flavors except Vanilla. 
 
-Hint: You can use .splice() for this
+Hint: You can use .splice() for this*/
 
-*/
-
-function removeFlavorByName(/*code here*/){
-
-    /*code here*/
-
+function removeFlavorByName(arr, string){
+    for( let i = 0; i < arr.length; i++){
+    if(arr[i] === string){
+    arr.splice(i, 1);
+    }
 }
-
+console.log(arr);
+}
+removeFlavorByName(originalFlavors, "Vanilla");
 
 /* Task 6: With all of these changes going on, we don't want to lose track of the actual, original 31 flavors. Write a function called copy that makes a copy of the array. 
 
@@ -124,12 +125,13 @@ Your function should accept:
 2 arguments 1 for your new array and one for your original array
 
 and should return a new array that is identical to the old array. You can name the new array however you'd like. */
-
-function copy(/*code here*/){
-
-    /*code here*/
-
+const newFlavor = []
+function copy(arr, newarr){
+   newarr = arr
+    console.log(newarr);
+    return newarr;
 }
+console.log(copy(originalFlavors, newFlavor));
 
 /* Task 7: July 7th is "World Chocolate Day" and Baskin Robins wants to create promotional materials highlighting all of their chocolate flavors. Write a function that checks every item in the array for a given string and returns a new array called filteredArray with just these values. Rather than hardcoding "chocolate" into your function, pass a string as a parameter, and invoke with the argument "chocolate". This way you could also filter for "Vanilla", "Sherbert", etc. when those holidays roll around.
 
